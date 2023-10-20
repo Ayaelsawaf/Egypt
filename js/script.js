a1fea3e72db6ff8111772ccfb342b9b1a5850db0
Ayaelsawaf <aelsawaf67@gmail.com>
2023-10-20 12:29:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("bt1").onclick = function () {
+    alert("copeid!")
+}
+document.getElementById("bt2").onclick = function () {
+    alert("copeid!")
+}
+document.getElementById("bt3").onclick = function () {
+    alert("copeid!")
+}
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+document.getElementById("login").onclick = function () {
+    prompt("if you want to login please fill up the form ok/No")
+    prompt("enter your name")
+    prompt("enter your email")
+    prompt("enter your password")
+    prompt("enter your phone")
+}
